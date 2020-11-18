61adee40bea61ec994e47b891e1af4e2f7309f10
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-18 11:16:45 +0530

    props updated

--- codes/day-3/react-props-state-demo/src/Welcome.js
+++ codes/day-3/react-props-state-demo/src/Welcome.js
@@ -1,47 +1,26 @@
-//import { Component } from 'react'
+import { Component } from 'react'
 
-/*
 class Welcome extends Component {
+
+    callPrint = () => {
+        this.props.print(100);
+    }
+
     render() {
-        return <span>Welcome to class component</span>
+        //console.log(this.props)
+        const { messageValue, data, show } = this.props;
+        //this.props.data = this.props.data + 10; <-- not possible (properties are immutable)
+        return (
+            <div>
+                Message:&nbsp;{messageValue}
+                <br />
+                Data:&nbsp;{data}
+                <br />
+                Show?&nbsp;{show.toString()}
+                <br />
+                <button onClick={this.callPrint}>Call Print</button>
+            </div>
+        )
     }
-}
-//console.log(Component.prototype)
-*/
-/**
- * properties={ messageValue:'Welcome to functional component', data:10, show:true, print:()=>{}}
- */
-const Welcome = (properties) => {
-    //properties.data = properties.data + 10;-->not possible
-    //let x = properties.data + 10;-->possible
-    const someValue = 100;
-    const callPrint = () => {
-        properties.print(someValue);
-    }
-    return (
-        <span>
-            Message:&nbsp;{properties.messageValue}
-            <br />
-            Data:&nbsp;{properties.data}
-            <br />
-            Show?&nbsp;{properties.show.toString()}
-            <br />
-            <button onClick={callPrint}>Call Print</button>
-        </span>
-    );
 }
 export default Welcome;
-/**
- * {
- * type:'span',
- * key:null,
- * ref:null,
- * props:{
- *   messageValue:'',
- *   data:10,
- *   show:true,
- *   print:()=>{},
- *   children:[RE1,....,RE13]
- * }
- * }
- */
